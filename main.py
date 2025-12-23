4757e7f87da57ed6095c290f9515cc943c993217
philips <philipsjose2003@gmail.com>
2025-12-23 14:01:00 +0530

    refactored the functionality to draw boxes to a separate function

--- main.py
+++ main.py
@@ -11,10 +11,24 @@ input = "inputs/image2.jpg"
 output = "outputs/output.jpg"
 outputText = "outputs/outputTextfile.txt"
 
-def draw_boxes(image, boxes, save_path="outputs/raw_boxes.png"):
+def draw_boxes(image: np.typing.NDArray, boxes: np.typing.NDArray, save_path="outputs/words_output.png"):
     img = image.copy()
-    boxes = np.concatenate(boxes, axis=0)
     for b in boxes:
+        # b = np.concatenate(b, axis=0)
+        x_min = int(b[:, 0].min())
+        y_min = int(b[:, 1].min())
+        x_max = int(b[:, 0].max())
+        y_max = int(b[:, 1].max())
+
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+
+    cv2.imwrite(save_path, img)
+    print(f"[+] Saved raw box image: {save_path}")
+
+def draw_boxes_of_lines(image: np.typing.NDArray, boxes: list, save_path="outputs/lines_output.png"):
+    img = image.copy()
+    for b in boxes:
+        b = np.concatenate(b, axis=0)
         x_min = int(b[:, 0].min())
         y_min = int(b[:, 1].min())
         x_max = int(b[:, 0].max())
@@ -47,17 +61,17 @@ if __name__ == "__main__":
     # Merge words into lines
     lines = merge_boxes_into_lines(boxes)
 
-    draw_boxes(image, lines)
+    # draw boxes
+    draw_boxes(image, boxes)
+    draw_boxes_of_lines(image, lines)
 
     recognized_lines = []
 
-    # Draw boxes and recognize text
+    # recognize text
     for line_box in lines:
         line_array = np.concatenate(line_box, axis=0)
         x_min, y_min = line_array[:,0].min(), line_array[:,1].min()
         x_max, y_max = line_array[:,0].max(), line_array[:,1].max()
-
-        cv2.rectangle(image, (int(x_min),int(y_min)), (int(x_max),int(y_max)), (0,255,0), 2)
 
         # Crop line for recognition
         line_img = image[int(y_min):int(y_max), int(x_min):int(x_max)]
@@ -66,8 +80,6 @@ if __name__ == "__main__":
         print("Recognized:", text)
         recognized_lines.append(text)
 
-    cv2.imwrite(output, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-    print("Output saved as output.jpg")
-
     #save results
     save_text(outputText, "\n".join(recognized_lines))
+    print("Output saved as outputTextfile.txt")
